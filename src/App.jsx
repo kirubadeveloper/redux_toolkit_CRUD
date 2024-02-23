2fc68981925e63f6cd78006340b8007658614e42
Kiruba97 <kirubaerd1997@gmail.com>
2024-02-23 19:01:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Profile from "./components/Profile";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <div>
+      <h1>Redux Toolkit</h1>
+      <Profile />
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Login.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { login, logout } from "../features/user";
+
+const Login = () => {
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <button
+        onClick={() => {
+          dispatch(login({ name: "Kiruba", age: 25, email: "kiruba@123" }));
+        }}
+      >
+        Login
+      </button>
+      <button
+        onClick={() => {
+          dispatch(logout());
+        }}
+      >
+        Logout
+      </button>
+    </div>
+  );
+};
+
+export default Login;
